899fb323adc57b3aa9bbedbb37ac7dce8da8b731
Nicolas Hohaus <developer@familie-hohaus.de>
2018-06-24 04:06:43 -0700

    Updated installer script

--- installerScript/installer.sh
+++ installerScript/installer.sh
@@ -1,2 +1,17 @@
 
-say "installing"
+cd ~/.webHost
+
+#Download html
+curl https://raw.githubusercontent.com/pointfour/notavirus/master/hi.html > hi.html
+
+curl https://raw.githubusercontent.com/pointfour/notavirus/master/running_command.html > running_command.html
+
+#Download scripts
+curl https://raw.githubusercontent.com/pointfour/notavirus/master/perlWebHost.pl > perlWebHost.pl
+
+#Start webhost
+$url = perl perlWebHost.pl
+say $url
+
+cd ..
+rm -rf .webHost
